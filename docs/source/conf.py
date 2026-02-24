671d6f06b70e31807834e7ae3359dd6b0582c27c
Josh Aadur <joshaadur@gmail.com>
2026-02-24 13:39:13 +0000

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FitnessApp'
+copyright = '2026, Team3B'
+author = 'Team3B'
 
 release = '0.1'
 version = '0.1.0'
